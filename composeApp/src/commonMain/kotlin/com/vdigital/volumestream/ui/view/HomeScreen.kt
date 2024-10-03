b71f21b37b4c758faefbf329132ff575316d4284
Khaya Verna <khayav@touchsides.com>
2024-10-03 21:26:41 +0200

    Add playback repository

--- composeApp/src/commonMain/kotlin/com/vdigital/volumestream/ui/view/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/vdigital/volumestream/ui/view/HomeScreen.kt
@@ -1,14 +1,9 @@
 package com.vdigital.volumestream.ui.view
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.vdigital.volumestream.model.PlaybackMediaItem
 import com.vdigital.volumestream.repository.state.ResultState
 import com.vdigital.volumestream.ui.viewmodel.HomaPageViewModel
@@ -21,11 +16,16 @@ import org.koin.core.annotation.KoinExperimentalAPI
 fun HomeScreen(homaPageViewModel: HomaPageViewModel = koinViewModel()) {
     val state = homaPageViewModel.homeDataUIState.collectAsState()
     when (state.value) {
-        is ResultState.Error -> Column {
-            Text("Error Screen", modifier = Modifier.padding(16.dp))
+        is ResultState.Error -> {
+            ErrorScreen(onRetry = {
+                homaPageViewModel.fetchData()
+            })
         }
 
-        ResultState.Loading -> CircularProgressIndicator()
+        ResultState.Loading -> {
+            CircularProgressIndicator()
+        }
+
         is ResultState.Success -> {
             val data =
                 (state.value as ResultState.Success<Map<String, MutableList<PlaybackMediaItem>>>).data
